64c8ff0028e4aa6f9feefdb67ea9a46b67dc32c9
Yichuan Shen <shen.yichuan@gmail.com>
2017-07-30 22:06:28 +0200

    Mention CC-CEDICT in the introduction

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -7,7 +7,11 @@ export default function Introduction() {
         <p>{[
             'This is a tool that can tokenize Chinese texts into words using ',
             <a href="https://github.com/yishn/chinese-tokenizer">chinese-tokenizer</a>,
-            '. Just paste your text down below:'
+            ' and ',
+            <a href="https://cc-cedict.org/">CC-CEDICT</a>,
+            '.'
         ]}</p>
+
+        <p>Just paste your text down below:</p>
     </section>
 }
