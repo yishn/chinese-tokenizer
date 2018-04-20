a841e0a3fdd0af191dfd4e8aeddb0946576c7755
Yichuan Shen <shen.yichuan@gmail.com>
2018-04-20 21:55:07 +0200

    Rename & prepare files

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
-const tokenizer = require('./index')('./cedict.txt')
-
+const tokenizer = require('.')('./cedict.txt')
 console.log(tokenizer.tokenize('我是中国人。'))
+
+const tokenizer = require('.')('./cedict.txt', 'traditional')
+console.log(tokenizer.tokenize('我是中國人。'))
